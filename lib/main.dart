3de0e1b0df6d5079030b0a019f9f36c6c85864bd
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2020-06-20 18:59:37 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/tonyStartLogo1.png'),
+                ),
+                Text(
+                  'Tony Stark',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                  ),
+                ),
+                Text(
+                  'IRON MAN',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal[100],
+                    fontFamily: 'Source San Pro',
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 123 456 7890',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'tonyTheStark@StarkIndustries.com',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
+        ),
+      ),
+    );
+  }
+}
